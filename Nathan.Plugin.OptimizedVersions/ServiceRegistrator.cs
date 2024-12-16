cae759d56c82eaef6616d007345859bfbe606063
lostb1t <coding-mosses0z@icloud.com>
2024-12-16 11:13:20 +0100

    fix: dbcontext

--- Nathan.Plugin.OptimizedVersions/ServiceRegistrator.cs
+++ Nathan.Plugin.OptimizedVersions/ServiceRegistrator.cs
@@ -1,30 +1,27 @@
-using System;
 using MediaBrowser.Controller;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Nathan.Plugin.OptimizedVersions.Data;
 using Nathan.Plugin.OptimizedVersions.Services;
+using MediaBrowser.Controller.Plugins;
+using Microsoft.EntityFrameworkCore;
 
 namespace Nathan.Plugin.OptimizedVersions;
 
 /// <summary>
 /// Provides service registration for the OptimizedVersions plugin.
 /// </summary>
-public static class ServiceRegistrator
+public class ServiceRegistrator : IPluginServiceRegistrator
 {
-    /// <summary>
-    /// Registers required services for the OptimizedVersions plugin.
-    /// </summary>
-    /// <param name="services">The service collection.</param>
-    /// <returns>The service collection for chaining.</returns>
-    public static IServiceCollection RegisterOptimizedVersionsServices(this IServiceCollection services)
+    /// <inheritdoc />
+    public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
     {
-        services.AddDbContextFactory<OptimizedVersionsDbContext>();
-        services.AddScoped<IOptimizedVersionsDbService, OptimizedVersionsDbService>();
-        services.AddSingleton<TranscodingService>();
-        services.AddSingleton<ITranscodingService>(sp => sp.GetRequiredService<TranscodingService>());
-        services.AddHostedService(sp => sp.GetRequiredService<TranscodingService>());
-
-        return services;
+        serviceCollection.AddDbContextFactory<OptimizedVersionsDbContext>(options =>
+            options.UseSqlite("Data Source=jellyfin.db"),
+            ServiceLifetime.Singleton
+        );
+        serviceCollection.AddScoped<IOptimizedVersionsDbService, OptimizedVersionsDbService>();
+        serviceCollection.AddSingleton<TranscodingService>();
+        serviceCollection.AddSingleton<ITranscodingService>(sp => sp.GetRequiredService<TranscodingService>());
+        serviceCollection.AddHostedService(sp => sp.GetRequiredService<TranscodingService>());
     }
 }
